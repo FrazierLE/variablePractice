9be4a149df54bb464e0da0be880cd135463506e4
Lauren Frazier <lauren.e.frazier@gmail.com>
2022-07-07 21:49:26 -0600

    Add variables

---
+++ variables.js
@@ -0,0 +1,11 @@
+## Strings
+var email = "lauren.e.frazier@gmail.com"
+var username = "FrazierLE"
+
+## Numbers
+var birthYear = "1994"
+var glassesOfWaterPerDay = "8"
+
+##Boolean
+var turingPass = true
+var turingFail = false
